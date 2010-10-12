1a5eb913d69ef4d87874332d73292090faf03a40
Dmitry Zuikov <dzuikov@gmail.com>
2010-10-12 11:03:42 +0400

    в процессе

--- src/repogen.ml
+++ src/repogen.ml
@@ -7,11 +7,17 @@ open Report_model
 
 module Db = Db_pg
 module T = Templating.Templating
+module B = Report_builder
 
 open Printf
 
 let parse_channel ch =
   let lex = Message.lexer_from_channel "stdin" ch
+  in let ast = Parser.toplevel Lexer.token lex
+  in ast
+
+let parse_string s = 
+  let lex = Message.lexer_from_string s ~fn:(Some("STDIN"))
   in let ast = Parser.toplevel Lexer.token lex
   in ast
 
@@ -24,8 +30,36 @@ let dump_output report out =
     | STDOUT    -> output_string Pervasives.stdout out
     | FILE(s)   -> Std.output_file s out
 
+type opt = { mutable opt_filename: string option;
+             mutable opt_output: output_t option;
+           }
+
+let report_of = function
+    | { opt_filename = None }    -> parse_channel (Pervasives.stdin)
+    | { opt_filename = Some(x) } -> parse_string (Std.input_file x)
+
+
+let with_options opts rep  =
+   let out r = match opts.opt_output with
+               | None    -> r 
+               | Some(x) -> { r with output = x }
+
+   in let fns = 
+        out :: []
+
+   in List.fold_left (fun acc f -> f acc) rep fns
+
+
 let () =
-    let report = parse_channel (Pervasives.stdin)
+    let opts = { opt_filename = None; opt_output = None }
+    in let _ = Arg.parse [
+                            ("--out-file",   Arg.String(fun s -> opts.opt_output <- Some(FILE(s))) , "set output file name");
+                            ("--out-stdout", Arg.Unit(fun ()  -> opts.opt_output <- Some(STDOUT) ) , "set output file to STDOUT");
+                            ("--define",     Arg.String(fun s -> ()) , "define variable NAME=VALUE");
+                         ] (fun x -> opts.opt_filename <- Some(x) ) "Usage:"
+
+
+    in let report = with_options opts (report_of opts)
 
     in let cache = T.cache ()
 
@@ -37,7 +71,7 @@ let () =
             
             in let data = Db.with_connection (fun conn -> Db.select_all conn sql (fun ds -> list_of_ds report ds))
                                                                               (connection_of report)
-            in let model = Model.make (column_headers report) data ([("SQL", sql)] @ (metavars report))
+            in let model = Model.make (column_headers report) data (metavars report)
 
             in match report.template with 
                | Some(s) ->
