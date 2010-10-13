a93eb49f3c499731b23e2b3378041b666c23b490
Dmitry Zuikov <dzuikov@gmail.com>
2010-10-13 23:15:15 +0400

    CSV auto generation

--- src/report_model.ml
+++ src/report_model.ml
@@ -2,6 +2,8 @@ module Model =
 struct
 
     open ExtList
+    open ExtHashtbl
+    open Util
     open CamlTemplate.Model
 
     let make row_hdr row_data vars =
@@ -11,9 +13,17 @@ struct
             in let () = List.iter (fun (n,v) -> Hashtbl.add hash n (Tstr v)) items
             in (Thash hash)
 
+        (* FIXME: fix column order *)
+        in let column_list ~args =
+         match args with
+             | [ Thash h ] -> (Tlist (List.of_enum (Hashtbl.values h)))
+             | _ -> raise (Tfun_error "Invalid argument")
+
         in let root = Hashtbl.create 10
         in let _ = Hashtbl.add root "header" (make_hash row_hdr)
+        in let _ = Hashtbl.add root "columns" (Tlist (List.map  (fun (a,b) -> make_hash [("name", a);("display_name",b)]) row_hdr))
         in let _ = Hashtbl.add root "rows"   (Tlist (List.map make_hash row_data))
+        in let _ = Hashtbl.add root "row_columns" (Tfun column_list)
         in let _ = List.iter (fun (n,v) -> Hashtbl.add root n (Tstr v)) vars
         in root
 
