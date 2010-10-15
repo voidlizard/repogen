52372889b57ccaefa564931071d7689ad1a60e54
Dmitry Zuikov <dzuikov@gmail.com>
2010-10-15 11:16:40 +0400

    row's column list in model for dynamic reports

--- src/report_model.ml
+++ src/report_model.ml
@@ -13,17 +13,13 @@ struct
             in let () = List.iter (fun (n,v) -> Hashtbl.add hash n (Tstr v)) items
             in (Thash hash)
 
-        (* FIXME: fix column order *)
-        in let column_list ~args =
-         match args with
-             | [ Thash h ] -> (Tlist (List.of_enum (Hashtbl.values h)))
-             | _ -> raise (Tfun_error "Invalid argument")
+        in let make_tlist row = Tlist(List.map (fun s -> Tstr(snd s)) row)
 
         in let root = Hashtbl.create 10
         in let _ = Hashtbl.add root "header" (make_hash row_hdr)
         in let _ = Hashtbl.add root "columns" (Tlist (List.map  (fun (a,b) -> make_hash [("name", a);("display_name",b)]) row_hdr))
         in let _ = Hashtbl.add root "rows"   (Tlist (List.map make_hash row_data))
-        in let _ = Hashtbl.add root "row_columns" (Tfun column_list)
+        in let _ = Hashtbl.add root "row_columns"  (Tlist (List.map make_tlist row_data))
         in let _ = List.iter (fun (n,v) -> Hashtbl.add root n (Tstr v)) vars
         in root
 
